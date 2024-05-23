e85d9bd00edcf582741e5f8f03336f5ab24c4b53
Kamal Sagar <kamalsagar316@gmail.com>
2024-05-23 18:30:01 +0530

    Create a boiler plate for project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h2 className="text-center text-blue-400">StartUp Axis</h2>
+    </div>
+  );
+}
+
+export default App;
